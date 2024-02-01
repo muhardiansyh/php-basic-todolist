d52130084032ff8e470fc09765781d1bcf6f32d9
yanhgx <mohammadd.arrdiansyahh@gmail.com>
2024-02-01 14:39:52 +0700

    create logic for ViewRemoveTodo

--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,7 +1,22 @@
 <?php 
 
+require_once "../Helper/input.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
 function viewRemoveTodoList(){
-    
+    echo "Menghapus TODO" . PHP_EOL;
+
+    $pilihan = input("Nomer (x) untuk batal");
+    if($pilihan == "x"){
+        echo "Batal menghapus todo";
+    } else {
+        $success = removeTodoList($pilihan);
+
+        if($success){
+            echo "Berhasil menghapus todo nomor: $pilihan" . PHP_EOL;
+        } else {
+            echo "Gagal menghapus todo nomor: $pilihan" . PHP_EOL;
+        }
+    }
 }
 
 ?>
