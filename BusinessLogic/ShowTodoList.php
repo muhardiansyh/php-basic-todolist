aa0e15082a78c6b94398c70ad77ed089822c53c0
yanhgx <mohammadd.arrdiansyahh@gmail.com>
2024-02-01 13:19:05 +0700

    create logic ShowTodoList

--- BusinessLogic/ShowTodoList.php
+++ BusinessLogic/ShowTodoList.php
@@ -3,7 +3,13 @@
 
 // show todo
 function showTodoList(){
+    global $todoList;
 
+    echo "TODOLIST" . PHP_EOL;
+
+    foreach($todoList as $number => $value){
+        echo "$number. $value" . PHP_EOL;
+    }
 }
 
 ?>
---
+++ Test/TestShowTodoList.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+
+showTodoList();
+
+?>
