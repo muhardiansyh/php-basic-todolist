5dd9bd36858c08e77dfe12c49c4b03cdea05abb3
yanhgx <mohammadd.arrdiansyahh@gmail.com>
2024-02-01 14:26:16 +0700

    create logic for input todo and ViewShowTodo

--- View/ViewShowTodoList.php
+++ View/ViewShowTodoList.php
@@ -1,7 +1,33 @@
 <?php 
 
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../View/ViewAddTodoList.php";
+require_once "../View/ViewRemoveTodoList.php";
+require_once "../Helper/input.php";
+
 function viewShowTodoList(){
-    
+    while(true){
+        showTodoList();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah todo" . PHP_EOL;
+        echo "2. Hapus todo" . PHP_EOL;
+        echo "3. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih");
+        if($pilihan == "1") {
+            viewAddTodoList();
+        } else if($pilihan == "2"){
+            viewRemoveTodoList();
+        } else if($pilihan == "x"){
+            break;
+        } else {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
 
 ?>
